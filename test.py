c03471f93bba7ea1accfcd9a6c878a30f51fdb82
Ofir Cohen <ofircohen2005@gmail.com>
2020-02-06 21:08:54 +0200

    add dataset to repo

--- test.py
+++ test.py
@@ -23,17 +23,57 @@ def extract_data(file):
 
 
 def eliminate_unimportant_features(dataset):
-    '''
-    Parameters: dataset with X columns
-    
-    Return value: dataset with Y columns after removing unimportant features (like constants etc.) 
-    '''
+	'''
+	Parameters: dataset with X columns
+	
+	Return value: dataset with Y columns after removing unimportant features (like constants etc.) 
+	'''
+
+
+def split_train_test(df, outcomes, train_percent=0.7):
+	'''
+	Parameters: dataset, outcome classes, train percent of dataset
+	
+	Return value: train set, test set
+	'''
+	train_size = []
+	df_outcomes = []
+	for outcome in outcomes:
+		df_outcomes.append(df.loc[df['AREA'] == outcome])
+		train_size.append(int(len(df.loc[df['AREA'] == outcome]) * train_percent))
+	
+	df_train = pd.DataFrame()
+	df_test = pd.DataFrame()
+	
+	for i in range(len(df_outcomes)):
+		df_train = df_train.append(df_outcomes[i][:train_size[i]])
+		df_test = df_test.append(df_outcomes[i][train_size[i]:])
+	
+	
+	X_train = np.asarray(df_train.drop('AREA', 1))
+	y_train = np.asarray(df_train['AREA'])
+	
+	X_test = np.asarray(df_test.drop('AREA', 1))
+	y_test = np.asarray(df_test['AREA'])
+	
+	# X_train = rescale_data(X_train)
+	# X_test = rescale_data(X_test)
+	
+	return X_train, X_test, y_train, y_test
+	''' End function '''
+
+
+def rescale_data(data):
+	'''
+	Rescale data to standard distribution
+	'''
+	return StandardScaler().fit_transform(data)
 
 
 def main():
-    df = extract_data('Crime_Data_from_2010_to_Present.csv')
-    df = eliminate_unimportant_features(df)
-    print(df)
+	df = extract_data('./dataset/nasa.csv')
+	df = shuffle(df)
+	print(df)
 
 
 if __name__ == "__main__":
