efa843a245dfb4ca5ef3352af75922de14cf6522
Ofir Cohen <ofircohen2005@gmail.com>
2020-02-13 20:38:36 +0200

    used SMOTE alogrithm to balance the dataset, split to train and test dataset

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 ############ IMPORTS #############
 import numpy as np
+from collections import Counter
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.utils import shuffle
 from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+from imblearn.over_sampling import SMOTE
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
@@ -14,53 +17,45 @@ import sys
 
 def extract_data(file):
 	'''
-	Parameters: xlsx file 
+	Parameters: path to a csv file with dataset 
 	
-	Return value: data from xlsx file as pandas object
+	Return value: data from csv file as pandas object
 	'''
 	df = pd.read_csv(file)
+	df.drop(['Neo Reference ID', 'Name', 'Close Approach Date', 'Epoch Date Close Approach', 'Orbiting Body', 'Orbit ID', 'Orbit Determination Date', 'Equinox'], 1, inplace=True)
 	return df
 
 
-def eliminate_unimportant_features(dataset):
+def split_matrix_vector(dataset):
 	'''
-	Parameters: dataset with X columns
+	Parameters: dataset with outcome column
 	
-	Return value: dataset with Y columns after removing unimportant features (like constants etc.) 
+	Return value: X Matrix, y Vector
 	'''
+	# print(dataset)
+	y = np.asarray(dataset['Hazardous'])
+	X = np.asarray(dataset.drop('Hazardous',1))
+	return X, y
 
 
-def split_train_test(df, outcomes, train_percent=0.7):
+def shuffle_and_split_train_test(X, y, features):
 	'''
-	Parameters: dataset, outcome classes, train percent of dataset
+	Parameters: X-Matrix, y-Vector, Matrix features
 	
-	Return value: train set, test set
+	Return value: Train dataset and Test dataset
 	'''
-	train_size = []
-	df_outcomes = []
-	for outcome in outcomes:
-		df_outcomes.append(df.loc[df['AREA'] == outcome])
-		train_size.append(int(len(df.loc[df['AREA'] == outcome]) * train_percent))
+	obj = { }
+	for i in range(len(features)-1):
+		print("feature: {}".format(features[i]))
+		print("values: {}".format(X[:, i]))
+		obj[features[i]] = X[:, i]
 	
-	df_train = pd.DataFrame()
-	df_test = pd.DataFrame()
-	
-	for i in range(len(df_outcomes)):
-		df_train = df_train.append(df_outcomes[i][:train_size[i]])
-		df_test = df_test.append(df_outcomes[i][train_size[i]:])
-	
-	
-	X_train = np.asarray(df_train.drop('AREA', 1))
-	y_train = np.asarray(df_train['AREA'])
-	
-	X_test = np.asarray(df_test.drop('AREA', 1))
-	y_test = np.asarray(df_test['AREA'])
-	
-	# X_train = rescale_data(X_train)
-	# X_test = rescale_data(X_test)
-	
+	obj[len(features)] = y[i]
+	df = pd.DataFrame(obj)
+	df = shuffle(df)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 	return X_train, X_test, y_train, y_test
-	''' End function '''
+
 
 
 def rescale_data(data):
@@ -72,8 +67,16 @@ def rescale_data(data):
 
 def main():
 	df = extract_data('./dataset/nasa.csv')
-	df = shuffle(df)
-	print(df)
+	features = df.columns.values
+	print(features)
+
+	# Split to X-Matrix and y-Vector
+	X, y = split_matrix_vector(df)
+	print("Original dataset shape: {}".format(Counter(y)))
+	sm = SMOTE(random_state=42)
+	X_res, y_res = sm.fit_resample(X, y)
+	print("Resampled dataset shape: {}".format(Counter(y_res)))
+	X_train, X_test, y_train, y_test = shuffle_and_split_train_test(X_res, y_res, features)
 
 
 if __name__ == "__main__":
